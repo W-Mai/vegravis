678e76c5807ead4f8b814523560528b1a5c64bfa
W-Mai <1341398182@qq.com>
2023-08-15 01:00:40 +0800

    🚧(gen): refactor VecLineGen to accept generic commands
    Changes were made in VecLineGen to replace VecOps enums with Command<f64> for vector operations. This intends to improve the code by eliminating duplication and enhancing readability. Removed specific add functions for VecOps as they are redundant now.
    
    Signed-off-by: W-Mai <1341398182@qq.com>

--- src/common_vec_op/gen.rs
+++ src/common_vec_op/gen.rs
@@ -30,8 +30,14 @@ impl VecLineGen {
     pub fn new(ops: Vec<Command<f64>>) -> Self {
         Self { ops }
     }
+}
 
-    fn gen(&self, range: Range<i64>) -> Vec<Vec<[f64; 2]>> {
+impl IVisDataGenerator<f64, f64, VecLineData> for VecLineGen {
+    fn add(&mut self, op: Command<f64>) {
+        self.ops.push(op);
+    }
+
+    fn gen(&self, range: Range<i64>) -> Vec<Vec<VecLineData>> {
         let mut cursor = PlotPoint::new(0.0, 0.0);
         let mut points_total = Vec::new();
         let mut points = Vec::new();
@@ -45,15 +51,15 @@ impl VecLineGen {
                 "MOVE" => {
                     cursor = PlotPoint::from([op.argv[0], op.argv[1]]);
                     if points_total.len() == 0 {
-                        points.push([0.0, 0.0]);
+                        points.push(VecLineData::new(0.0, 0.0));
                     }
                     points_total.push(points);
                     points = Vec::new();
-                    points.push([op.argv[0], op.argv[1]]);
+                    points.push(VecLineData::new(op.argv[0], op.argv[1]));
                 }
                 "LINE" => {
-                    points.push([cursor.x, cursor.y]);
-                    points.push([op.argv[0], op.argv[1]]);
+                    points.push(VecLineData::new(op.argv[0], op.argv[1]));
+                    points.push(VecLineData::new(op.argv[0], op.argv[1]));
                     cursor = PlotPoint::from([op.argv[0], op.argv[1]]);
                 }
                 "QUAD" => {
@@ -62,7 +68,7 @@ impl VecLineGen {
                     while t < 1.0 {
                         let x = (1.0f64 - t).powi(2) * cursor.x + 2.0 * (1.0 - t) * t * x1 + t.powi(2) * x2;
                         let y = (1.0f64 - t).powi(2) * cursor.y + 2.0 * (1.0 - t) * t * y1 + t.powi(2) * y2;
-                        points.push([x, y]);
+                        points.push(VecLineData::new(x, y));
                         t += 0.01;
                     }
                     cursor = PlotPoint::from([x2, y2]);
@@ -73,7 +79,7 @@ impl VecLineGen {
                     while t < 1.0 {
                         let x = (1.0f64 - t).powi(3) * cursor.x + 3.0 * (1.0 - t).powi(2) * t * x1 + 3.0 * (1.0 - t) * t.powi(2) * x2 + t.powi(3) * x3;
                         let y = (1.0f64 - t).powi(3) * cursor.y + 3.0 * (1.0 - t).powi(2) * t * y1 + 3.0 * (1.0 - t) * t.powi(2) * y2 + t.powi(3) * y3;
-                        points.push([x, y]);
+                        points.push(VecLineData::new(x, y));
                         t += 0.01;
                     }
                     cursor = PlotPoint::from([x3, y3]);
@@ -91,27 +97,6 @@ impl VecLineGen {
         }
 
         points_total
-    }
-}
-
-impl IVisDataGenerator<f64, f64, VecLineData> for VecLineGen {
-    fn add(&mut self, op: Command<f64>) {
-        self.ops.push(op);
-    }
-
-    fn gen(&self, range: Range<i64>) -> Vec<Vec<VecLineData>> {
-        let data = self.gen(range);
-        let mut ret = Vec::new();
-
-        for line in data {
-            let mut vec_line_datas = Vec::new();
-            for pos in line {
-                vec_line_datas.push(VecLineData::new(pos[0], pos[1]));
-            }
-            ret.push(vec_line_datas);
-        }
-
-        ret
     }
 
     fn len(&self) -> usize {
