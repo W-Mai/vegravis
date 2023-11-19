6db9c4a2a5f7aa1d913eadc6aa5c68c3174e345d
Benign X <1341398182@qq.com>
2023-11-20 03:00:57 +0800

    😁(Lib): refactor code by moving modules and constants to lib.rs
    Moved multiple modules and the color palette constant from main.rs into lib.rs. This change improves code organization by isolating modules and constants within a dedicated library file, promoting better modularity and readability.
    
    Signed-off-by: Benign X <1341398182@qq.com>

---
+++ src/lib.rs
@@ -0,0 +1,24 @@
+mod app;
+mod common_vec_op;
+mod cus_component;
+mod interfaces;
+mod any_data;
+
+pub use eframe::egui;
+pub use app::MainApp;
+
+
+// beautiful colors
+// c08eaf, fba414, 8cc269, 4f9da6, 9b5c5a, 5a5c9b, 9b5a5c, 5c9b5a, 5c9b9b, 9b5c9b
+const COLOR_PALETTE: [egui::Color32; 10] = [
+    egui::Color32::from_rgb(0xc0, 0x8e, 0xaf),
+    egui::Color32::from_rgb(0xfb, 0xa4, 0x14),
+    egui::Color32::from_rgb(0x8c, 0xc2, 0x69),
+    egui::Color32::from_rgb(0x4f, 0x9d, 0xa6),
+    egui::Color32::from_rgb(0x9b, 0x5c, 0x5a),
+    egui::Color32::from_rgb(0x5a, 0x5c, 0x9b),
+    egui::Color32::from_rgb(0x9b, 0x5a, 0x5c),
+    egui::Color32::from_rgb(0x5c, 0x9b, 0x5a),
+    egui::Color32::from_rgb(0x5c, 0x9b, 0x9b),
+    egui::Color32::from_rgb(0x9b, 0x5c, 0x9b),
+];
--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,13 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
-mod app;
-mod common_vec_op;
-mod cus_component;
-mod interfaces;
-mod any_data;
-
-use eframe::egui;
-use crate::app::MainApp;
-
-
-// beautiful colors
-// c08eaf, fba414, 8cc269, 4f9da6, 9b5c5a, 5a5c9b, 9b5a5c, 5c9b5a, 5c9b9b, 9b5c9b
-const COLOR_PALETTE: [egui::Color32; 10] = [
-    egui::Color32::from_rgb(0xc0, 0x8e, 0xaf),
-    egui::Color32::from_rgb(0xfb, 0xa4, 0x14),
-    egui::Color32::from_rgb(0x8c, 0xc2, 0x69),
-    egui::Color32::from_rgb(0x4f, 0x9d, 0xa6),
-    egui::Color32::from_rgb(0x9b, 0x5c, 0x5a),
-    egui::Color32::from_rgb(0x5a, 0x5c, 0x9b),
-    egui::Color32::from_rgb(0x9b, 0x5a, 0x5c),
-    egui::Color32::from_rgb(0x5c, 0x9b, 0x5a),
-    egui::Color32::from_rgb(0x5c, 0x9b, 0x9b),
-    egui::Color32::from_rgb(0x9b, 0x5c, 0x9b),
-];
+use vegravis::MainApp;
 
 // When compiling natively:
 #[cfg(not(target_arch = "wasm32"))]
 fn main() -> Result<(), eframe::Error> {
     env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
     let options = eframe::NativeOptions {
-        initial_window_size: Some(egui::vec2(1440.0, 960.0)),
+        initial_window_size: Some(eframe::egui::vec2(1440.0, 960.0)),
         ..Default::default()
     };
     eframe::run_native(
